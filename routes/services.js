dc2b98201e016c8e974329338ebdb9091ac7ac02
Axel Garcia <axel.garcia94@gmail.com>
2018-09-22 15:39:53 +0200

    create services views and routes

--- routes/services.js
+++ routes/services.js
@@ -16,6 +16,33 @@ router.get('/', (req, res, next) => {
     .catch(next);
 });
 
+router.get('/create', (req, res, next) => {
+  const formData = req.flash('service-form-data');
+  const formErrors = req.flash('service-form-error');
+  const data = {
+    message: formErrors[0],
+    fields: formData[0]
+  };
+  res.render('service-create', data);
+});
+
+router.post('/create', (req, res, next) => {
+  const { name, description, time, category } = req.body;
+
+  if (!name || !description || !time || !category) {
+    req.flash('service-form-error', 'all fields are mandatory');
+    req.flash('service-form-data', { name, description, time, category });
+    return res.redirect('/services/create');
+  }
+
+  const service = new Service({ name, description, time, category });
+  service.save()
+    .then(() => {
+      res.redirect('/services');
+    })
+    .catch(next);
+});
+
 router.get('/:serviceId', (req, res, next) => {
   const id = req.params.serviceId;
   Service.findById(id)
