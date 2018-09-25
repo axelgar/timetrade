b86657cea335b3aa0d737107998d973e3d73bd76
Axel Garcia <axel.garcia94@gmail.com>
2018-09-25 20:02:55 +0200

    adding pictures

--- data/services.js
+++ data/services.js
@@ -6,28 +6,28 @@ const services = [{
   description: 'I offer lessons prepared specially for you, according to your needs! I can help you to communicate, to write and to read in portuguese, so that you can have a complete experience in Brazil.',
   time: 60,
   category: 'languages',
-  url: 'https://images.unsplash.com/photo-1512932503744-92c247fc46e5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6b5241837b5f11c6f7dd9f926c081640&auto=format&fit=crop&w=1567&q=80'
+  url: '../public/images/Portgues.jpeg'
 }, {
   owner: ['Gabriela'],
   name: 'Painting class',
   description: 'Private class with a professional teacher',
   time: 30,
   category: 'arts',
-  url: 'https://images.unsplash.com/photo-1520420097861-e4959843b682?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7a76802ff83a847d60d945e4f44cd241&auto=format&fit=crop&w=1650&q=80'
+  url: '../public/images/Art.jpeg'
 }, {
   owner: ['Axel'],
   name: 'Visit to secret places in Barcelona',
   description: 'Route to the secret places around Barcelona with a local',
   time: 60,
   category: 'social',
-  url: 'https://cope-cdnmed.agilecontent.com/resources/jpg/3/7/1535096328773.jpg'
+  url: '../public/images/Barcelona.jpg'
 }, {
   owner: ['Guille'],
   name: 'Yoga',
   description: 'Private class with a professional yoga teacher with 10 years of experience',
   time: 60,
   category: 'sports',
-  url: 'https://images.unsplash.com/photo-1508919801845-fc2ae1bc2a28?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=de3a8532e9be6e0d3f35c15a471ed569&auto=format&fit=crop&w=1685&q=80f'
+  url: '../public/images/Yoga.jpeg'
 }];
 
 module.exports = services;
